bd44b90fa28f9badf004f75d5b3ff6a1bcd17bd5
chusov.e <chusoveg17@gmail.com>
2021-11-22 20:32:02 +0500

    Добавил библиотеку

---
+++ Photoshop.Core/Models/Image.cs
@@ -0,0 +1,15 @@
+﻿namespace Photoshop.Core.Models;
+
+public class Image
+{
+    public int Width { get; }
+    public int Height { get; }
+    private Pixel[,] pixels; //TODO: зависимость от пикселей нехорошо,можно переделать на какую-нибудь абстракццию
+
+    public Image(Pixel[,] pixels)
+    {
+        this.pixels = pixels;
+        Width = pixels.GetLength(0);
+        Height = pixels.GetLength(1);
+    }
+}
---
+++ Photoshop.Core/Models/Pixel.cs
@@ -0,0 +1,15 @@
+﻿namespace Photoshop.Core.Models;
+
+public class Pixel
+{
+    public double R { get; }
+    public double G { get; }
+    public double B { get; }
+
+    public Pixel(double r, double g, double b)
+    {
+        R = r;
+        G = g;
+        B = b;
+    }
+}
